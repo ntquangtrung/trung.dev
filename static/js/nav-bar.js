79656f426c5e86d469b86fefdb85c0022b3bb507
Trung Nguyen <ng.qtrung95@gmail.com>
2025-07-15 16:05:24 +0700

    improve navbar & darkmode logic

--- static/js/nav-bar.js
+++ static/js/nav-bar.js
@@ -1,16 +1,32 @@
 $(function () {
-  $("#toggle-sidebar-desktop, #toggle-sidebar-mobile").on("click", function () {
-    $("#nav-sidebar, #toggle-sidebar-desktop, #main-content").toggleClass(
-      "open"
-    );
-  });
-
-  $("#toggle-darkmode").on("click", function () {
-    $("html").toggleClass("dark");
-    if ($("html").hasClass("dark")) {
-      localStorage.setItem("darkmode", "enabled");
-    } else {
-      localStorage.setItem("darkmode", "disabled");
-    }
-  });
+  if (localStorage.getItem("sidebar") === "open") {
+    console.log("calling me...");
+    $(
+      "#nav-sidebar, #main-content, #toggle-sidebar-desktop, #toggle-sidebar-mobile"
+    ).addClass("open");
+  }
 });
+
+const darkModeKey = "darkmode";
+
+function toggleDarkMode() {
+  $("html").toggleClass("dark");
+  if ($("html").hasClass("dark")) {
+    localStorage.setItem(darkModeKey, "enabled");
+  } else {
+    localStorage.setItem(darkModeKey, "disabled");
+  }
+}
+
+const navKey = "sidebar";
+
+function toggleNav() {
+  this.classList.toggle("open");
+  $("#nav-sidebar, #main-content").toggleClass("open");
+  // Update localStorage based on sidebar state
+  if ($("#nav-sidebar").hasClass("open")) {
+    localStorage.setItem(navKey, "open");
+  } else {
+    localStorage.setItem(navKey, "closed");
+  }
+}
