b5d619daf55fc971c2dce1822660bd0c5f66806f
Trung Nguyen <ng.qtrung95@gmail.com>
2025-08-12 16:36:30 +0700

    improve admin site

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -6,6 +6,10 @@ from apps.blog.model_admin.resume import ResumeAdmin
 
 from apps.blog.models import User, NotesToSelf, Profile, Resume
 
+admin.site.site_header = "Blog Admin"
+admin.site.site_title = "Blog Admin"  # This is the HTML <title>
+admin.site.index_title = "Blog Admin"
+
 # Register your models here.
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(NotesToSelf, NoteAdmin)
