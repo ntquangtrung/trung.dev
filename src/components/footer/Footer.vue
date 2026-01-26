c139bb9c20ef2a7198e1f3b4cc5f8fbdd232703f
Trung Nguyen <ng.qtrung95@gmail.com>
2026-01-26 16:41:16 +0700

    add author

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import styles from "./styles.module.css";
 </script>
 
@@ -7,6 +9,6 @@ import styles from "./styles.module.css";
     <strong class="text-gradient text-2xl font-pixeboy leading-[unset] select-none"
       >Made by Trung</strong
     >
-    <small>Một sản phẩm của Trung</small>
+    <small><FontAwesomeIcon :icon="faHeart" /> trung.dev</small>
   </footer>
 </template>
